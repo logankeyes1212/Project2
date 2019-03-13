b3e1f26d7d36c153f576734ca8aac821bd53103c
Logan Geoffrion <45701086+Lgeoffrion@users.noreply.github.com>
2019-03-12 23:08:50 -0500

    working side nav

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,24 @@ $(document).ready(function(){
   $('.tabs').tabs();
   $('.sidenav').sidenav();
   $(".dropdown-trigger").dropdown();
+  // $(".button-collapse").sideNav({
+  //   closeOnClick: false
+  // });
+
+
+  var elem = document.querySelector('.sidenav');
+  var instance = M.Sidenav.init(elem, {
+      onOpenStart: function () {
+      },
+      onCloseEnd: function () {
+        $('.tabs').tabs();
+      }
+  });
+
+// instance.updateTabIndicator();
+// instance.select('.tabs');
+  
   });
 
 
-  var instance = M.Tabs.getInstance(elem);
-
-  instance.select('tab_id');
-
-  instance.updateTabIndicator();
+ 
