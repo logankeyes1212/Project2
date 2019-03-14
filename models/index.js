5ee31d735e7c9a375e5eb6754d78210d8a6b3bd8
Logan <logankeyes1212@gmail.com>
2019-03-14 11:57:55 -0500

    dont approve this

--- models/index.js
+++ models/index.js
@@ -6,7 +6,9 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
+// var config    =  require(__dirname + '/../config/config.js')[env];
 var db        = {}; //name of database here
+
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
